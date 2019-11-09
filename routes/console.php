edd03a6014596228682c310f4726d67a11d5403f
Jasper van Rijbroek <jasper@jvar.nl>
2019-11-09 09:53:34 +0100

    Added cli command.

--- routes/console.php
+++ routes/console.php
@@ -16,3 +16,7 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+Artisan::command('import-rates', function() {
+    \App\Utils\Currencies\Currencies::importRates();
+})->describe('Import the conversion rates');
